352f101587028e3b25e44630b2b38d6065b16e69
jona <jona@elevation.ac>
2020-02-18 11:58:51 +0200

    sc1 explanation

--- filters-and-sorts/spot-check-1.sql
+++ filters-and-sorts/spot-check-1.sql
@@ -4,3 +4,5 @@ WHERE
   product_brand = 'Gorilla' AND
     (product_cost < 1 OR
     recyclable = 'TRUE')
+-- Note the importance of the parenthese in this case! 
+-- Without parentheses here, the query would return 875 rows because the OR 'overrides' the AND condition
