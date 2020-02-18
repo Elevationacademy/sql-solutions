f9ec65677d9e0c7056fc6c36fdd40d4d28eb3d62
jona <jona@elevation.ac>
2020-02-18 15:24:15 +0200

    sc3 explanation

--- filters-and-sorts/spot-check-3.sql
+++ filters-and-sorts/spot-check-3.sql
@@ -1,6 +1,9 @@
 SELECT
-  TOP 30 product_cost,
+  TOP 30 
+  product_cost,
   product_weight,
   product_cost / product_weight AS cpw
 FROM Products
-ORDER BY cpw DESC
+ORDER BY cpw DESC -- the 'cpw' is the alias we just created!
+
+-- notice that in this case we split the SELECT statement for readability
